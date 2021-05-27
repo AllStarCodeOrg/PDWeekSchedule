3d6745d968470a70656f12c839230d033aeb36ee
danteasc4 <zexovon@gmail.com>
2021-05-27 13:44:33 -0400

    First commit

---
+++ src/components/AllMatsLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  type link = {
+    displayText: string;
+    linkURL: string;
+  };
+  export let actname: string;
+  export let links: link[];
+</script>
+
+<li class="collection-item">
+  <h5>{actname}</h5>
+  {#each links as link}
+    <a class="gen-link" target="_blank" href={link.linkURL}>{link.displayText}</a>
+  {/each}
+</li>
